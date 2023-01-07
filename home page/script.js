45c3f9b8a9885513a9153395ffbf67c20983341a
shaba-imran <imranusmanshaba@gmail.com>
2023-01-07 17:20:28 +0100

    Refactor: refactor code for side navigation

--- home page/script.js
+++ home page/script.js
@@ -27,62 +27,36 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-// Side Navigation
+
+// SIDE NAVIGATION
 let sideNav = document.getElementById("mySidenav");
 
-// Open Side Navigation
-function openNav() {
-  document.getElementById("mySidenav").style.width = "350px";
-
-  function reduceSideNavWidth1(viewport1) {
-    if (viewport1.matches) { // If media query matches
-      sideNav.style.width = "250px";
-    }
+const changeSideNavWidth = (currentViewPort, newWidth) => {
+  if (currentViewPort.matches) { // If media query matches
+    sideNav.style.width = newWidth;
   }
+}
+
+// Open Side Navigation
+const openNav = () => {
+  sideNav.style.width = "350px";
 
   let viewport1 = window.matchMedia("(max-width: 535px)");
-  reduceSideNavWidth1(viewport1); // Call listener function at run time
-  viewport1.addListener(reduceSideNavWidth1); // Attach listener function on state changes
-
-
-  function reduceSideNavWidth2(viewport2) {
-    if (viewport2.matches) {
-      sideNav.style.width = "200px";
-    }
-  }
+  changeSideNavWidth(viewport1, '250px'); // Call listener function at run time
+  viewport1.addListener(changeSideNavWidth); // Attach listener function on state changes
 
   let viewport2 = window.matchMedia("(max-width: 320px)");
-  reduceSideNavWidth2(viewport2);
-  viewport2.addListener(reduceSideNavWidth2);
+  changeSideNavWidth(viewport2, '200px'); // Call listener function at run time
+  viewport2.addListener(changeSideNavWidth); // Attach listener function on state changes
 }
 
 // Close Side Navigation
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-
-  function closeSideNav1(viewport1) {
-    if (viewport1.matches) { // If media query matches
-      sideNav.style.width = "0";
-    }
-  }
-
-  let viewport1 = window.matchMedia("(min-width: 536px)");
-  closeSideNav1(viewport1); // Call listener function at run time
-  viewport1.addListener(closeSideNav1); // Attach listener function on state changes
-
-
-  function closeSideNav2(viewport2) {
-    if (viewport2.matches) {
-      sideNav.style.width = "0";
-    }
-  }
-
-  let viewport2 = window.matchMedia("(min-width: 321px)");
-  closeSideNav2(viewport2);
-  viewport2.addListener(closeSideNav2);
+const closeNav = () => {
+  sideNav.style.width = "0";
 }
 
-// Chat Section
+
+// CHAT SECTION
 // const students = document.getElementsByClassName("students");
 // const prevButton = document.getElementById("myBtn1");
 // const nextButton = document.getElementById("myBtn2"); 
