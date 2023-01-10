488a4b0e0e914566bc2c356d4968939490f1cedc
shaba-imran <imranusmanshaba@gmail.com>
2023-01-10 18:25:08 +0100

    Chore: add dropdown for side navigation

--- home page/script.js
+++ home page/script.js
@@ -29,17 +29,19 @@ function showSlides(n) {
 
 
 // SIDE NAVIGATION
-let sideNav = document.getElementById("mySidenav");
+const getElementById = (value) => {
+  return document.getElementById(value);
+}
 
 const setSideNavWidth = (width) => {
-  sideNav.style.width = width;
+  getElementById('mySidenav').style.width = width;
 }
 
 const openSideNav = () => {
   setSideNavWidth("350px");
 
   const reduceSideNavWidth = (viewport) => {
-    if (viewport.matches) {
+    if (viewport.matches) { // If media query matches
       if (viewport.media === "(max-width: 535px)") {
         setSideNavWidth("250px");
       } else if (viewport.media === "(max-width: 320px)") {
@@ -49,8 +51,8 @@ const openSideNav = () => {
   }
 
   let viewport1 = window.matchMedia("(max-width: 535px)");
-  reduceSideNavWidth(viewport1);
-  viewport1.addListener(reduceSideNavWidth);
+  reduceSideNavWidth(viewport1); // Call listener function at run time
+  viewport1.addListener(reduceSideNavWidth); // Attach listener function on state changes
 
   let viewport2 = window.matchMedia("(max-width: 320px)");
   reduceSideNavWidth(viewport2);
@@ -60,6 +62,141 @@ const openSideNav = () => {
 const closeSideNav = () => {
   setSideNavWidth("0");
 }
+
+const setMarginValue = (idName, value) => {
+  getElementById(idName).style.marginBottom = value;
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+const openFirstDropDown = () => {
+  getElementById('myFirstDropdown').classList.toggle("show");
+  setMarginValue('firstDropdown', '730px');
+  const changeMarginValue = (viewport) => {
+    if (viewport.matches) {
+      if (viewport.media === "(max-width: 535px)") {
+        setMarginValue('firstDropdown', '780px');
+      } else if (viewport.media === "(max-width: 450px)") {
+        setMarginValue('firstDropdown', '725px');
+      } else if (viewport.media === "(max-width: 320px)") {
+        setMarginValue('firstDropdown', '760px');
+      }
+    }
+  }
+  let viewport1 = window.matchMedia("(max-width: 535px)");
+  changeMarginValue(viewport1);
+  viewport1.addListener(changeMarginValue);
+
+  let viewport2 = window.matchMedia("(max-width: 450px)");
+  changeMarginValue(viewport2);
+  viewport2.addListener(changeMarginValue);
+
+  let viewport3 = window.matchMedia("(max-width: 320px)");
+  changeMarginValue(viewport3);
+  viewport3.addListener(changeMarginValue);
+}
+
+const openSecondDropDown = () => {
+  getElementById('mySecondDropdown').classList.toggle("show");
+  setMarginValue('secondDropdown', '720px');
+  const changeMarginValue = (viewport) => {
+    if (viewport.matches) {
+      if (viewport.media === "(max-width: 535px)") {
+        setMarginValue('secondDropdown', '825px');
+      } else if (viewport.media === "(max-width: 450px)") {
+        setMarginValue('secondDropdown', '720px');
+      } else if (viewport.media === "(max-width: 320px)") {
+        setMarginValue('secondDropdown', '830px');
+      }
+    }
+  }
+  let viewport1 = window.matchMedia("(max-width: 535px)");
+  changeMarginValue(viewport1);
+  viewport1.addListener(changeMarginValue);
+
+  let viewport2 = window.matchMedia("(max-width: 450px)");
+  changeMarginValue(viewport2);
+  viewport2.addListener(changeMarginValue);
+
+  let viewport3 = window.matchMedia("(max-width: 320px)");
+  changeMarginValue(viewport3);
+  viewport3.addListener(changeMarginValue);
+}
+
+const openThirdDropDown = () => {
+  getElementById('myThirdDropdown').classList.toggle("show");
+  setMarginValue('thirdDropdown', '570px');
+  const changeMarginValue = (viewport) => {
+    if (viewport.matches) {
+      if (viewport.media === "(max-width: 535px)") {
+        setMarginValue('thirdDropdown', '630px');
+      } else if (viewport.media === "(max-width: 450px)") {
+        setMarginValue('thirdDropdown', '580px');
+      } else if (viewport.media === "(max-width: 320px)") {
+        setMarginValue('thirdDropdown', '625px');
+      }
+    }
+  }
+  let viewport1 = window.matchMedia("(max-width: 535px)");
+  changeMarginValue(viewport1);
+  viewport1.addListener(changeMarginValue);
+
+  let viewport2 = window.matchMedia("(max-width: 450px)");
+  changeMarginValue(viewport2);
+  viewport2.addListener(changeMarginValue);
+
+  let viewport3 = window.matchMedia("(max-width: 320px)");
+  changeMarginValue(viewport3);
+  viewport3.addListener(changeMarginValue);
+}
+
+const openFourthDropDown = () => {
+  getElementById('myFourthDropdown').classList.toggle("show");
+  setMarginValue('fourthDropdown', '300px');
+  const changeMarginValue = (viewport) => {
+    if (viewport.matches) {
+      if (viewport.media === "(max-width: 535px)") {
+        setMarginValue('fourthDropdown', '310px');
+      } else if (viewport.media === "(max-width: 450px)") {
+        setMarginValue('fourthDropdown', '285px');
+      } else if (viewport.media === "(max-width: 320px)") {
+        setMarginValue('fourthDropdown', '320px');
+      }
+    }
+  }
+  let viewport1 = window.matchMedia("(max-width: 535px)");
+  changeMarginValue(viewport1);
+  viewport1.addListener(changeMarginValue);
+
+  let viewport2 = window.matchMedia("(max-width: 450px)");
+  changeMarginValue(viewport2);
+  viewport2.addListener(changeMarginValue);
+
+  let viewport3 = window.matchMedia("(max-width: 320px)");
+  changeMarginValue(viewport3);
+  viewport3.addListener(changeMarginValue);
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("Dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        setMarginValue('firstDropdown', '0');
+        setMarginValue('secondDropdown', '0');
+        setMarginValue('thirdDropdown', '0');
+        setMarginValue('fourthDropdown', '0');
+      }
+    }
+  }
+}
+
+
+
+
 
 
 // Chat Section
