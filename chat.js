0839f94164f0cdbb92fd469230800a0382f7fdfb
Imran Usman Shaba <imranusmanshaba@gmail.com>
2023-01-11 08:20:26 +0100

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,14 @@
+// CHAT WITH STUDENTS 
+// const students = document.getElementsByClassName("students");
+// const prevButton = document.getElementById("myBtn1");
+// const nextButton = document.getElementById("myBtn2"); 
+  
+// const removeStudent = () => {
+//   let student = document.getElementsByClassName("student");
+//   for (let i=0; i < student.length; i++) {
+//     student[i].remove();
+//   }
+  
+// }
+
+// nextButton.addEventListener("click", removeStudent);
