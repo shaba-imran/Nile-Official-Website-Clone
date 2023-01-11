fbabc18e4a0157a7c68a4f958ecda3d896a6b99e
shaba-imran <imranusmanshaba@gmail.com>
2023-01-11 17:31:23 +0100

    Fix: fix the slideshow on the home page

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -1,19 +1,22 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-const plusSlides = (n) => showSlides(slideIndex += n);
+const plusSlides = (n) => { showSlides(slideIndex += n); }
 
-const currentSlide = (n) => showSlides(slideIndex = n);
+const currentSlide = (n) => { showSlides(slideIndex = n); }
 
-const showSlides = (n) => {
+function showSlides(n) {
   let i;
-  const slides = document.getElementsByClassName("mySlides");
-  const dots = document.getElementsByClassName("dot");
-  slideIndex = n > slides.length ? 1 : n < 1 ? slides.length : slideIndex;
-  Array.from(slides).forEach((slide, index) => {
-    slide.style.display = index + 1 === slideIndex ? 'block' : 'none';
-  });
-  Array.from(dots).forEach((dot, index) => {
-    dot.className = index + 1 === slideIndex ? dot.className + " active" : dot.className.replace(" active", "");
-  });
-};
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
